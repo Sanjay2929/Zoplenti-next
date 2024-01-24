2ecf5117109bb7c09dcbb123cf2bd09378e79660
Naveen <naveenninu888@gmail.com>
2024-01-24 14:02:20 +0530

    first commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,10 @@
 import React, { useState } from "react";
 import Header from "../common/header/Header";
 import Hero from "./Hero";
+import PartnersList from "./../common/json/PartnersList"
+
+// import DistributorPartners from "../common/partners/DistributorPartners";
+import Brands from "../common/partners/Brands";
 
 const HomePage = () => {
   const [isBannerVisible, setIsBannerVisible] = useState(false);
@@ -12,6 +16,8 @@ const HomePage = () => {
         setIsBannerVisible={setIsBannerVisible}
       />
       <Hero />
+      <Brands brands={PartnersList} show="all" margin_bottom="mb-[-40px] sm:mb-[-80px]" padding_top="md:pt-[100px] lg:pt-[120px]" />
+
     </>
   );
 };
